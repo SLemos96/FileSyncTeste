7984356f87a27056b2c963fdd186e80f0df62dbb
samue <samue@10.5.31.115>
2016-04-05 10:12:44 -0300

    primeiro comentario

---
+++ src/filesync/cliente/Reply.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filesync.cliente;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.Socket;
+
+/**
+ *
+ * @author Reinaldo
+ */
+public class Reply implements Serializable{
+
+    public Reply(File caminho, InetAddress ip, int port) {
+
+        try {
+            int size = (int)caminho.length();
+            byte[] resposta = new byte[size];
+            
+            Socket sock = new Socket(ip, port);
+            
+            ObjectOutputStream out = new ObjectOutputStream(sock.getOutputStream());
+
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("arquivo não encontrado");
+        }  catch (IOException ex3) {
+            System.out.println("falha ao enviar arquivo");
+        }
+        
+
+    }
+
+}
